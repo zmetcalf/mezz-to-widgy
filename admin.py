c04cbcc139dbef613bcd84aeb1aea00b07f8b18b
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-05-20 09:18:57 -0600

    Added Aaron Ghent's suggestion

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from mezzanine.pages.admin import PageAdmin
 from mezzanine.pages.models import RichTextPage
 
-admin.site.register(RichTextPage)
+admin.site.register(RichTextPage, PageAdmin)
